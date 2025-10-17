f057a79b73aa4f469ff831677d384d29938caeb5
tangbin0524 <2168374471@qq.com>
2025-10-17 20:52:05 +0800

    test::Add and Pass all move command test cases

--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -50,4 +50,40 @@ TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
     const Pose target({1, 0, 'E'});  // 命令正确时的姿势
     ASSERT_EQ(target, executor->Query());
 }
+TEST(ExecutorTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W)
+{
+    // given 给定一个executor
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+
+    // when 调用executor的Execute方法去执行命令
+    executor->Execute("M");
+
+    // then
+    const Pose target({-1, 0, 'W'});  // 命令正确时的姿势
+    ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
+{
+    // given 给定一个executor
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+
+    // when 调用executor的Execute方法去执行命令
+    executor->Execute("M");
+
+    // then
+    const Pose target({0, 1, 'N'});  // 命令正确时的姿势
+    ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
+{
+    // given 给定一个executor
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+
+    // when 调用executor的Execute方法去执行命令
+    executor->Execute("M");
+
+    // then
+    const Pose target({0, -1, 'S'});  // 命令正确时的姿势
+    ASSERT_EQ(target, executor->Query());
+}
 }  // namespace adas
