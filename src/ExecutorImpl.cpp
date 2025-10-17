6f8e83ebc9a0f63eaab5f6ce7acd524443bee84a
tangbin0524 <2168374471@qq.com>
2025-10-17 20:26:36 +0800

    feat:Add test cases for initial and default poses

---
+++ src/ExecutorImpl.cpp
@@ -0,0 +1,24 @@
+#include "ExecutorImpl.hpp"
+
+#include <new>
+
+namespace adas
+{
+ExecutorImpl::ExecutorImpl(const Pose &pose) noexcept
+{
+}
+
+Pose ExecutorImpl::Query(void) const noexcept
+{
+    return pose;
+}
+/*
+    std::nothrow 是C++标准库中的一个常量，用于指示在分配内存时不抛出任何异常。
+    它是std::nothrow_t 类型的实例，通常用在new运算符和std::nothrow命名空间中，
+    以请求内存分配器在分配失败是返回一个空指针，而不是抛出std::bad_alloc异常
+*/
+Executor *Executor::NewExecutor(const Pose &pose) noexcept
+{
+    return new (std::nothrow) ExecutorImpl(pose);
+}
+}  // namespace adas
---
+++ tests/ExecutorTest.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <tuple>
+
+#include "Executor.hpp"
+
+namespace adas
+{
+bool operator==(const Pose &lhs, const Pose &rhs)
+{
+    return std::tie(lhs.x, lhs.y, lhs.heading) == std::tie(rhs.x, rhs.y, rhs.heading);
+}
+
+// 测试用例1
+TEST(ExecutorTest, should_return_init_pose_when_without_command)
+{
+    // given给定测试条件
+    // 测试条件就是调用Executor的静态方法NewExecutor
+    // 返回一个指向Executor对象的只能指针executor，这样我们就不需要去delete了
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+    // when
+
+    // then
+    const Pose target({0, 0, 'E'});
+
+    ASSERT_EQ(target, executor->Query());
+}
+// 测试用例2
+TEST(ExecutorTest, should_return_default_pose_when_without_init_and_command)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor());
+    // when
+
+    // then
+    const Pose target({0, 0, 'N'});
+    ASSERT_EQ(target, executor->Query());
+}
+}  // namespace adas
